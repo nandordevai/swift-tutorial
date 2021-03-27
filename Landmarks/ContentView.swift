eca69fb0cc5e8f1632d200648e11fb2845f980b0
Nandor Devai <mail@nandordevai.net>
2021-03-27 22:14:52 +0100

    first tutorial

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,16 +1,35 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Nandor Devai on 2021. 03. 27..
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 350)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y: -180)
+                .padding(.bottom, -180)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                    .padding(.bottom, 1)
+                Text("It is held sacred by the Gabrieleño Native Americans, and is located in the northern part of the Turtle Rock neighborhood, near Concordia University, Irvine.")
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
