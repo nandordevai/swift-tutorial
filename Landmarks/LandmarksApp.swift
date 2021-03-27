f7dedcadbc1dd4be1c77d334704f4e11a2133cfe
Nandor Devai <mail@nandordevai.net>
2021-03-27 16:40:56 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nandor Devai on 2021. 03. 27..
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
